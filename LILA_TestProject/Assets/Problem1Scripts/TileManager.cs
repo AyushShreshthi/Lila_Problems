9ed081747e074e3925051bbc2abafcea970da613
ayush <ayushshreshthi41@gmail.com>
2021-07-16 13:47:37 +0530

    AdjacentTiles
    Adjacent  Tiles Population

--- LILA_TestProject/Assets/Problem1Scripts/TileManager.cs
+++ LILA_TestProject/Assets/Problem1Scripts/TileManager.cs
@@ -32,5 +32,191 @@ public class TileManager : MonoBehaviour
         int childnumber = go.transform.GetSiblingIndex();
         go.GetComponentInChildren<Text>().text = (childnumber + 1).ToString();
         go.GetComponent<Image>().color = colors[childnumber];
+
+
+        PopulateAdjacentUp(go);
+        PopulateAdjacentDown(go);
+        PopulateAdjacentRight(go);
+        PopulateAdjacentLeft(go);
+        PopulateAdjacentUpRight(go);
+        PopulateAdjacentUpLeft(go);
+        PopulateAdjacentDownRight(go);
+        PopulateAdjacentDownLeft(go);
+    }
+
+    public void PopulateAdjacentUp(GameObject go)
+    {
+        for (int i = 1; i <= areaOfInterest; i++)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(go.transform.position + (Vector3.up * 100f), Vector2.up, 300f);
+            Debug.DrawRay(go.transform.position, Vector2.up * 20f);
+            GameObject goup = new GameObject();
+            
+            if (hit.collider != null && hit.collider.transform.gameObject != go)
+            {
+                goup = hit.transform.gameObject;
+                int childnumberup = goup.transform.GetSiblingIndex();
+                goup.GetComponentInChildren<Text>().text = (childnumberup + 1).ToString();
+                goup.GetComponent<Image>().color = colors[childnumberup];
+            }
+            go = goup;
+        }
+        
+    }
+    public void PopulateAdjacentDown(GameObject go)
+    {
+        for (int i = 1; i <= areaOfInterest; i++)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(go.transform.position + (-Vector3.up * 100f), -Vector2.up, 300f);
+            Debug.DrawRay(go.transform.position, -Vector2.up * 20f);
+            GameObject goup = new GameObject();
+
+            if (hit.collider != null && hit.collider.transform.gameObject != go)
+            {
+                goup = hit.transform.gameObject;
+                int childnumberup = goup.transform.GetSiblingIndex();
+                goup.GetComponentInChildren<Text>().text = (childnumberup + 1).ToString();
+                goup.GetComponent<Image>().color = colors[childnumberup];
+            }
+            go = goup;
+        }
+
+    }
+    public void PopulateAdjacentRight(GameObject go)
+    {
+        for (int i = 1; i <= areaOfInterest; i++)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(go.transform.position + (Vector3.right * 100f), Vector2.right, 300f);
+            Debug.DrawRay(go.transform.position, Vector2.right * 20f);
+            GameObject goup = new GameObject();
+
+            if (hit.collider != null && hit.collider.transform.gameObject != go)
+            {
+                goup = hit.transform.gameObject;
+                int childnumberup = goup.transform.GetSiblingIndex();
+                goup.GetComponentInChildren<Text>().text = (childnumberup + 1).ToString();
+                goup.GetComponent<Image>().color = colors[childnumberup];
+            }
+            go = goup;
+        }
+
+    }
+    public void PopulateAdjacentLeft(GameObject go)
+    {
+        for (int i = 1; i <= areaOfInterest; i++)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(go.transform.position + (-Vector3.right * 100f), -Vector2.right, 300f);
+            Debug.DrawRay(go.transform.position, -Vector2.right * 20f);
+            GameObject goup = new GameObject();
+
+            if (hit.collider != null && hit.collider.transform.gameObject != go)
+            {
+                goup = hit.transform.gameObject;
+                int childnumberup = goup.transform.GetSiblingIndex();
+                goup.GetComponentInChildren<Text>().text = (childnumberup + 1).ToString();
+                goup.GetComponent<Image>().color = colors[childnumberup];
+            }
+            go = goup;
+        }
+
+    }
+
+    public void PopulateAdjacentUpRight(GameObject go)
+    {
+        for (int i = 1; i <= areaOfInterest; i++)
+        {
+            Vector3 direction =  go.transform.GetChild(1).transform.position-go.transform.position;
+            RaycastHit2D hit = Physics2D.Raycast(go.transform.GetChild(1).transform.position, direction, 100f);
+            Debug.DrawRay(go.transform.GetChild(1).transform.position, direction * 100f, Color.yellow);
+            GameObject goup = new GameObject();
+
+            if (hit.collider != null && hit.collider.transform.gameObject != go)
+            {
+                
+                goup = hit.transform.gameObject;
+                int childnumberup = goup.transform.GetSiblingIndex();
+                goup.GetComponentInChildren<Text>().text = (childnumberup + 1).ToString();
+                goup.GetComponent<Image>().color = colors[childnumberup];
+            }
+            else
+            {
+                break;
+            }
+            go = goup;
+        }
+    }
+    public void PopulateAdjacentUpLeft(GameObject go)
+    {
+        for (int i = 1; i <= areaOfInterest; i++)
+        {
+            Vector3 direction = go.transform.GetChild(2).transform.position - go.transform.position;
+            RaycastHit2D hit = Physics2D.Raycast(go.transform.GetChild(2).transform.position, direction, 100f);
+            Debug.DrawRay(go.transform.GetChild(2).transform.position, direction * 100f, Color.yellow);
+            GameObject goup = new GameObject();
+
+            if (hit.collider != null && hit.collider.transform.gameObject != go)
+            {
+
+                goup = hit.transform.gameObject;
+                int childnumberup = goup.transform.GetSiblingIndex();
+                goup.GetComponentInChildren<Text>().text = (childnumberup + 1).ToString();
+                goup.GetComponent<Image>().color = colors[childnumberup];
+            }
+            else
+            {
+                break;
+            }
+            go = goup;
+        }
+    }
+
+    public void PopulateAdjacentDownRight(GameObject go)
+    {
+        for (int i = 1; i <= areaOfInterest; i++)
+        {
+            Vector3 direction = go.transform.GetChild(3).transform.position - go.transform.position;
+            RaycastHit2D hit = Physics2D.Raycast(go.transform.GetChild(3).transform.position, direction, 100f);
+            Debug.DrawRay(go.transform.GetChild(3).transform.position, direction * 100f, Color.yellow);
+            GameObject goup = new GameObject();
+
+            if (hit.collider != null && hit.collider.transform.gameObject != go)
+            {
+
+                goup = hit.transform.gameObject;
+                int childnumberup = goup.transform.GetSiblingIndex();
+                goup.GetComponentInChildren<Text>().text = (childnumberup + 1).ToString();
+                goup.GetComponent<Image>().color = colors[childnumberup];
+            }
+            else
+            {
+                break;
+            }
+            go = goup;
+        }
+    }
+
+    public void PopulateAdjacentDownLeft(GameObject go)
+    {
+        for (int i = 1; i <= areaOfInterest; i++)
+        {
+            Vector3 direction = go.transform.GetChild(4).transform.position - go.transform.position;
+            RaycastHit2D hit = Physics2D.Raycast(go.transform.GetChild(4).transform.position, direction, 100f);
+            Debug.DrawRay(go.transform.GetChild(4).transform.position, direction * 100f, Color.yellow);
+            GameObject goup = new GameObject();
+
+            if (hit.collider != null && hit.collider.transform.gameObject != go)
+            {
+
+                goup = hit.transform.gameObject;
+                int childnumberup = goup.transform.GetSiblingIndex();
+                goup.GetComponentInChildren<Text>().text = (childnumberup + 1).ToString();
+                goup.GetComponent<Image>().color = colors[childnumberup];
+            }
+            else
+            {
+                break;
+            }
+            go = goup;
+        }
     }
 }
